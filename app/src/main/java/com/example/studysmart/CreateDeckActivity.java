002468cfa26970b074e14d5f89ef3f531fa8d69c
Eye Alpha <johnnykwok13@gmail.com>
2018-04-26 07:33:38 +0900

    new file:   .gitignore 	new file:   .idea/caches/build_file_checksums.ser 	new file:   .idea/codeStyles/Project.xml 	new file:   .idea/gradle.xml 	new file:   .idea/misc.xml 	new file:   .idea/runConfigurations.xml 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/example/studysmart/ExampleInstrumentedTest.java 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/example/studysmart/CreateDeckActivity.java 	new file:   app/src/main/java/com/example/studysmart/MainActivity.java 	new file:   app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   app/src/main/res/drawable/ic_launcher_background.xml 	new file:   app/src/main/res/layout/activity_create_deck.xml 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/ids.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/styles.xml 	new file:   app/src/test/java/com/example/studysmart/ExampleUnitTest.java 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle

---
+++ app/src/main/java/com/example/studysmart/CreateDeckActivity.java
@@ -0,0 +1,13 @@
+package com.example.studysmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateDeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_deck);
+    }
+}
---
+++ app/src/main/java/com/example/studysmart/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.studysmart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.StudySmart.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the button */
+    public void createDeck(View view) {
+        Intent intent = new Intent(this, CreateDeckActivity.class);
+        startActivity(intent);
+    }
+}
