c23c1c65909fe56e2244231991f250628b2a0220
Khelil <khelil87@gmail.com>
2018-05-06 15:04:47 -0400

    Add, Remove and ViewInOrder
    Fucntions added

--- app/src/main/java/com/example/studysmart/CreateDeckActivity.java
+++ app/src/main/java/com/example/studysmart/CreateDeckActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class CreateDeckActivity extends AppCompatActivity {
 
+    //below are the shared preference constants needed to save the user data
+    public static final String SHARED_PREFS = "shared_prefs";
+    public static final String DECK_NAME = "deck_name";
+    //cannot make the deck state static
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
