0599730dd527b402740751c804a39eb2d185f4b1
Khelil <khelil87@gmail.com>
2018-05-11 23:20:11 -0400

    Final Changes

--- app/src/main/java/com/example/studysmart/ViewDetailsActivity.java
+++ app/src/main/java/com/example/studysmart/ViewDetailsActivity.java
@@ -6,30 +6,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import static com.example.studysmart.MainActivity.fromStudy;
 import static com.example.studysmart.MainActivity.selectedDeck;
 
-public class DeckDetailsActivity extends AppCompatActivity {
+public class ViewDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deck_details);
+        setContentView(R.layout.activity_view_details);
         // NEED TO START ADDING CODE HERE
-        Button addNewFlashCardButton = findViewById(R.id.addFlashCardButton);
+        Button addCardButton = findViewById(R.id.addCardButton);
         Button studyButton = findViewById(R.id.studyButton);
 
-        TextView totalText = (TextView) findViewById(R.id.total);
-        TextView percentText = (TextView) findViewById(R.id.percentageTextView);
+        TextView totalText = (TextView) findViewById(R.id.totalTextView);
+        TextView percentEasyText = (TextView) findViewById(R.id.percentEasyTextView);
+        TextView percentMediumText = (TextView) findViewById(R.id.percentMediumTextView);
+        TextView percentHardText = (TextView) findViewById(R.id.percentHardTextView);
         TextView difficultyText = (TextView) findViewById(R.id.difficultyTextView);
 
         totalText.setText("" + selectedDeck.totalSize + " total cards");
+        //easy
         if (selectedDeck.easy_v.size() == 0)
         {
-            percentText.setText( "" + 0 + "%");
+            percentEasyText.setText( "" + 0 + "%");
         }
-        else { percentText.setText( "" + (selectedDeck.easy_v.size() / selectedDeck.totalSize)*100 + "%"); }
+        else { percentEasyText.setText( "" + (selectedDeck.easy_v.size() / selectedDeck.totalSize)*100 + "%"); }
+        //medium
+        if (selectedDeck.medium_v.size() == 0)
+        {
+            percentMediumText.setText( "" + 0 + "%");
+        }
+        else { percentMediumText.setText( "" + (selectedDeck.medium_v.size() / selectedDeck.totalSize)*100 + "%"); }
+        //hard
+        if (selectedDeck.hard_v.size() == 0)
+        {
+            percentHardText.setText( "" + 0 + "%");
+        }
+        else { percentHardText.setText( "" + (selectedDeck.hard_v.size() / selectedDeck.totalSize)*100 + "%"); }
 
         if(selectedDeck.easy_v.size() >= selectedDeck.medium_v.size() &&
                 selectedDeck.easy_v.size() >= selectedDeck.hard_v.size()){
@@ -48,10 +64,10 @@ public class DeckDetailsActivity extends AppCompatActivity {
         }
 
         // event handling
-        addNewFlashCardButton.setOnClickListener(
+        addCardButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        startActivity(new Intent(DeckDetailsActivity.this, AddCardToCurrentDeckActivity.class));
+                        startActivity(new Intent(ViewDetailsActivity.this, AddCardToCurrentDeckActivity.class));
                     }
                 }
         );
@@ -60,7 +76,7 @@ public class DeckDetailsActivity extends AppCompatActivity {
                 new Button.OnClickListener() {
                     public void onClick(View v) {
                         fromStudy = true;
-                        startActivity(new Intent(DeckDetailsActivity.this, ViewDeckActivity.class));
+                        startActivity(new Intent(ViewDetailsActivity.this, ViewDeckActivity.class));
                     }
                 }
         );
@@ -74,3 +90,4 @@ public class DeckDetailsActivity extends AppCompatActivity {
     }
 
 }
+
